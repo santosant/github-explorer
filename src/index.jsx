f9c42d4ab83a899218dfb62f24ee4d0035ce2d59
santosant <hiroto.anderson@gmail.com>
2021-03-09 13:36:02 -0300

    devSetup: babel, webpack, pckg.json, loaders

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App'
 
-import { Render } from 'react-dom'
-
-
+render(<App />, document.getElementById('root'))
 
